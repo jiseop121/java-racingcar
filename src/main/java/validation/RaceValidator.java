7d48198f9ad69f040335651ba9ab44fb1c26505d
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-27 21:30:17 +0900

    Feat(RaceValidator) : `RaceCount` 객체 검증 생성

--- src/main/java/validation/RaceValidator.java
+++ src/main/java/validation/RaceValidator.java
@@ -6,6 +6,9 @@ public class RaceValidator {
     private static final String COMMA = ",";
     private static final String DOUBLE_COMMA =",,";
 
+    private static final int MAX_RACE_COUNT = 1000;
+    private static final int ZERO = 0;
+
     public static void validateInputCars(String inputCars){
         checkDoubleComma(inputCars);
         checkStartOrEndWithComma(inputCars);
@@ -28,6 +31,23 @@ public class RaceValidator {
             Integer.parseInt(inputRaceCount.trim());
         }catch (Exception e){
             throw new IllegalArgumentException(INPUT_RACE_COUNT_INPUT_NUMBER.getMessage());
+        }
+    }
+
+    public static void validateRaceCount(int value){
+        checkIsMinus(value);
+        checkIsOverMax(value);
+    }
+
+    private static void checkIsOverMax(int value) {
+        if(value >MAX_RACE_COUNT){
+            throw new IllegalArgumentException(ErrorMessage.RACE_COUNT_NO_OVER_1000.getMessage());
+        }
+    }
+
+    private static void checkIsMinus(int value) {
+        if(value <ZERO){
+            throw new IllegalArgumentException(ErrorMessage.RACE_COUNT_NOT_MINUS.getMessage());
         }
     }
 
