34e18b2b14e01ac700f42c9955fc3a2ce9878570
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-27 21:31:22 +0900

    Test(RaceCountTest) : `RaceCount` 테스트

---
+++ src/test/java/model/RaceCountTest.java
@@ -0,0 +1,34 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static validation.ErrorMessage.RACE_COUNT_NOT_MINUS;
+import static validation.ErrorMessage.RACE_COUNT_NO_OVER_1000;
+
+import org.junit.jupiter.api.Test;
+
+class RaceCountTest {
+
+    @Test
+    void RaceCount_정상생성(){
+        int normalCount = 10;
+        RaceCount raceCount = new RaceCount(normalCount);
+        assertThat(raceCount.value()).isEqualTo(normalCount);
+    }
+
+    @Test
+    void RaceCount_음수_예외출력(){
+        int minusCount = -1;
+        assertThatThrownBy(() -> new RaceCount(minusCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(RACE_COUNT_NOT_MINUS.getMessage());
+    }
+
+    @Test
+    void RaceCount_너무높은수_1000이상_예외출력(){
+        int overThousand = 1001;
+        assertThatThrownBy(() -> new RaceCount(overThousand))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(RACE_COUNT_NO_OVER_1000.getMessage());
+    }
+}
