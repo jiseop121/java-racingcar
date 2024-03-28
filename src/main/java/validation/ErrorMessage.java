f60354f29428850b130a15a3a6614356f7ed4f51
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-28 10:51:01 +0900

    Feat(RaceValidator) : `CarName` 검증 및 예외 출력 기능

--- src/main/java/validation/ErrorMessage.java
+++ src/main/java/validation/ErrorMessage.java
@@ -2,8 +2,14 @@ package validation;
 
 public enum ErrorMessage {
     INPUT_RACE_COUNT_INPUT_NUMBER("숫자를 입력하세요"),
-    INPUT_CARS_COMMA_FORMAT("쉼표 작성 형식을 맞춰서 입력해주세요.");
+    INPUT_CARS_COMMA_FORMAT("쉼표 작성 형식을 맞춰서 입력해주세요."),
 
+    COUNT_NOT_MINUS("음수를 입력할 수 없습니다."),
+    RACE_COUNT_NO_OVER_1000("경주 횟수는 1000회를 넘을 수 없습니다."),
+
+    CAR_NAME_NO_OVER_MAX_LENGTH("자동차 이름은 5글자 이하로 작성해야 합니다."),
+    CAR_NAME_NO_BLANK("자동차 이름은 공백일 수 없습니다."),
+    CARS_HAS_DUPLICATED_NAMES("중복된 자동차 이름이 존재합니다.");
 
 
     private final String message;
--- src/main/java/validation/RaceValidator.java
+++ src/main/java/validation/RaceValidator.java
@@ -2,6 +2,11 @@ package validation;
 
 import static validation.ErrorMessage.*;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import model.Car;
+
 public class RaceValidator {
     private static final String COMMA = ",";
     private static final String DOUBLE_COMMA =",,";
@@ -9,9 +14,12 @@ public class RaceValidator {
     private static final int MAX_RACE_COUNT = 1000;
     private static final int ZERO = 0;
 
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+
     public static void validateInputCars(String inputCars){
         checkDoubleComma(inputCars);
         checkStartOrEndWithComma(inputCars);
+
     }
 
     private static void checkStartOrEndWithComma(String inputCars) {
@@ -26,7 +34,7 @@ public class RaceValidator {
         }
     }
 
-    public static void validateRaceCount(String inputRaceCount){
+    public static void validateInputRaceCount(String inputRaceCount){
         try{
             Integer.parseInt(inputRaceCount.trim());
         }catch (Exception e){
@@ -36,10 +44,10 @@ public class RaceValidator {
 
     public static void validateRaceCount(int value){
         checkIsMinus(value);
-        checkIsOverMax(value);
+        checkIsOverMaxRaceCount(value);
     }
 
-    private static void checkIsOverMax(int value) {
+    private static void checkIsOverMaxRaceCount(int value) {
         if(value >MAX_RACE_COUNT){
             throw new IllegalArgumentException(ErrorMessage.RACE_COUNT_NO_OVER_1000.getMessage());
         }
@@ -47,7 +55,45 @@ public class RaceValidator {
 
     private static void checkIsMinus(int value) {
         if(value <ZERO){
-            throw new IllegalArgumentException(ErrorMessage.RACE_COUNT_NOT_MINUS.getMessage());
+            throw new IllegalArgumentException(ErrorMessage.COUNT_NOT_MINUS.getMessage());
+        }
+    }
+
+
+    public static void validateCarName(String  value){
+        checkIsBlank(value);
+        checkIsOverMaxNameLength(value);
+    }
+
+    private static void checkIsOverMaxNameLength(String value) {
+        if (value.length()>MAX_CAR_NAME_LENGTH){
+            throw new IllegalArgumentException(CAR_NAME_NO_OVER_MAX_LENGTH.getMessage());
+        }
+    }
+
+    private static void checkIsBlank(String value) {
+        if(value.isBlank()){
+            throw new IllegalArgumentException(CAR_NAME_NO_BLANK.getMessage());
+        }
+    }
+
+    public static void validateRaceDistance(int value){
+        checkIsMinus(value);
+    }
+
+    public static void validateCars(List<Car> playCars) {
+        Set<String> carNames = new HashSet<>();
+        playCars.forEach(
+                car -> {
+                    checkIsDuplicatedCarName(car, carNames);
+                }
+        );
+    }
+
+    private static void checkIsDuplicatedCarName(Car car, Set<String> carNames) {
+        String carName = car.getCarName().value();
+        if (!carNames.add(carName)) {
+            throw new IllegalArgumentException(CARS_HAS_DUPLICATED_NAMES.getMessage());
         }
     }
 
