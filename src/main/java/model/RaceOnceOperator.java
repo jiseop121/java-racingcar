d66e8b90a2aaaad674bf5a3930cfe8dd43e7ab37
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-29 12:16:16 +0900

    Refactor(RaceOnceOperator) : `RaceCount` 주입 관계 및 validation 기능 추가

--- src/main/java/model/RaceOnceOperator.java
+++ src/main/java/model/RaceOnceOperator.java
@@ -1,23 +1,49 @@
 package model;
 
+import java.util.List;
 import model.domain.Car;
 import model.domain.Cars;
+import model.domain.RaceCount;
+import validation.ErrorMessage;
 
 public class RaceOnceOperator {
 
     private final NumberGenerator numberGenerator;
-    private static final int FORWARD_STANDARD_NUMBER= 4;
+    private final RaceCount raceCount;
 
-    public RaceOnceOperator(NumberGenerator numberGenerator) {
+    private static final int FORWARD_STANDARD_NUMBER = 4;
+
+    public RaceOnceOperator(NumberGenerator numberGenerator, RaceCount raceCount) {
         this.numberGenerator = numberGenerator;
+        this.raceCount = raceCount;
     }
 
     public void race(Cars cars){
+        validateRaceOnce(cars);
         cars.playCars().forEach(
                 car -> {
                     doForwardOrStop(car);
                 }
         );
+    }
+
+    private void validateRaceOnce(Cars cars) {
+        if(isEnd(cars)){
+            throw new IllegalArgumentException(ErrorMessage.RACE_ALREADY_OVER.getMessage());
+        }
+    }
+
+    public boolean isEnd(Cars cars){
+        for (Car car : cars.playCars()) {
+            if(isReachEndDistance(car)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isReachEndDistance(Car car) {
+        return raceCount.value() <= car.raceDistance().getValue();
     }
 
     private void doForwardOrStop(Car car) {
