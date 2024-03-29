b2c5b69101407afb30201d45f35870b310b29fd2
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-29 23:06:55 +0900

    Feat(ViewMessage) : 출력 문구 모음

---
+++ src/main/java/view/ViewMessage.java
@@ -0,0 +1,17 @@
+package view;
+
+public enum ViewMessage {
+    INPUT_CARS_MESSAGE("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."),
+    INPUT_RACE_COUNT_MESSAGE("시도할 회수는 몇회인가요?"),
+    RESULT_START_MESSAGE("최종결과"),
+    FINALLY_WIN("가 최종 우승했습니다.");
+
+    private final String message;
+    ViewMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
